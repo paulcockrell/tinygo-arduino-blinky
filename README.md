ccdfe620c279fcfce3509e6c8da1f5295a7abc81
Paul Cockrell <paulcockrell@gmail.com>
2020-07-04 13:15:16 +0100

    Add logging and update the readme to show how to connect to the arduino serial port

--- README.md
+++ README.md
@@ -5,3 +5,10 @@ Basic Arduino program written in TinyGo thats blinks the onboard LED
 ```
 $> make deploy
 ```
+
+## View output
+To view the output adjust the details below to suit the board settings.
+To exit the screen type `Ctrl-a k`
+```
+$> screen /dev/ttyACM0 9600
+```
--- main.go
+++ main.go
@@ -6,12 +6,16 @@ import (
 )
 
 func main() {
+	println("Let get blinky!")
+
 	led := machine.LED
 	led.Configure(machine.PinConfig{Mode: machine.PinOutput})
 	for {
+		println("LED off")
 		led.Low()
 		time.Sleep(time.Millisecond * 1000)
 
+		println("LED on")
 		led.High()
 		time.Sleep(time.Millisecond * 1000)
 	}
